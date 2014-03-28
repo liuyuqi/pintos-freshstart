fa75c05ca20973bd0443b6dae6ee6c5a5000f1d9
liuyuqi <liuyuqi.crh@gmail.com>
2014-03-28 18:18:45 -0400

    added changes to the shell, making less bugs possible by checking inputs and the memory

--- src/threads/shell.c
+++ src/threads/shell.c
@@ -4,9 +4,11 @@
  */
 
 #include "shell.h"
+#define MAX_CHAR_USER 32
+#define MAX_CHAR_INPUT 255
 
-char user[32];
-char input[255];
+char user[MAX_CHAR_USER];
+char cmd_input[MAX_CHAR_INPUT];
 
 /* The shell's entry function */
 int
@@ -15,14 +17,41 @@ shell(void)
     shell_init();
     printf("Pintos Shell started running!\n");
 
+	/* This is the iteration of shell execution */
     while (1) {
 	    printf("[%s@Pintos]# ", user);
-        getline(input);
-        printf("You just entered %s\n", input);
+        getline(cmd_input);
+        check_char_memory_bound(cmd_input, MAX_CHAR_INPUT);
         find_cmd();
+        prepare_for_next_cycle();
     }
 
     return 0;
+}
+
+/* This function returns 0 if the input is within bound,
+ *   and the actual length of the string. */
+int
+check_char_memory_bound(char* to_check, int size)
+{
+	int length = strlen(to_check);
+	if (length >= size) {
+		printf("sh: error: memory corrupted!\n");
+		printf("sh: accepted input was %d, actual input is %d!\n", size-1, length);
+		exit(0); /* I just exit to prevent harm to the system */
+	}
+	else return 0;
+}
+
+/* clean up the routinely used variables, creating a clean 
+ * environment for the next iteration of shell execution.
+ * This is EXTREMELY IMPORTANT in C programs, if you don't
+ * want to get into NASTY and WEIRD problems!
+ */
+void
+prepare_for_next_cycle(void)
+{
+	memset(cmd_input, 0, sizeof(cmd_input));
 }
 
 /* The initialization function of the shell */
@@ -32,10 +61,20 @@ shell_init(void)
 	strlcpy(user, "root", 5);
 }
 
-/* Find the internal command to execute */
+/* Find the internal command (and probably out-source cmds later)
+ * to execute */
 void
 find_cmd(void)
 {
-	if (strcmp(input, "exit") == 0)
+	/* input was an empty command, doing nothing */
+	if (strcmp(cmd_input, "") == 0) 
+		return; 
+		
+	printf("You just entered %s\n", cmd_input);
+	/* start searching for known commands */
+	if (strcmp(cmd_input, "exit") == 0)
 	    exit(0);
+	else {
+		printf("Command %s not found!\n", cmd_input);
+	}
 }
--- src/threads/shell.h
+++ src/threads/shell.h
@@ -19,4 +19,23 @@ shell_init(void);
 void
 find_cmd(void);
 
+/* clean up the routinely used variables,
+ * creating a clean environment for the next
+ * iteration of shell execution.
+ */
+void
+prepare_for_next_cycle(void);
+
+/* This checks whether input is within bound limit of a string or
+ * an array of chars. The C library of pintos didn't check memory
+ * corruption, therefore as Yuqi's observation we can freely 
+ * modify lots of memory after the blocks allocated for the 
+ * string. The function is to prevent this, which can be a 
+ * disaster, from happening.
+ * 
+ * Also, if anyone want to perform this check, it's always fun to
+ * implement this functionality in the Pintos C library, which I 
+ * believe is a great practise!!! */
+int
+check_char_memory_bound(char* to_check, int size);
 #endif
